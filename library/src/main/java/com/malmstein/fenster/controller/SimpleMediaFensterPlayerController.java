f5284fd946e083807ed8b0b9d13b1ab02b92660d
yangfeng <yangfeng@91taogu.com>
2016-01-13 18:41:23 +0800

    call super onFinishInflate

--- library/src/main/java/com/malmstein/fenster/controller/SimpleMediaFensterPlayerController.java
+++ library/src/main/java/com/malmstein/fenster/controller/SimpleMediaFensterPlayerController.java
@@ -84,6 +84,7 @@ public final class SimpleMediaFensterPlayerController extends FrameLayout implem
 
     @Override
     protected void onFinishInflate() {
+        super.onFinishInflate();
         LayoutInflater.from(getContext()).inflate(R.layout.fen__view_simple_media_controller, this);
         initControllerView();
     }
